1f3bb1a702152309d0229ff24e38d3a6aaebd862
Tapan_CS <mahatatapan2000@gmail.com>
2023-06-07 22:10:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function addSpan(cls,spanE1,xPos,yPos){
     spanE1.classList.add(cls);
     spanE1.style.left=xPos+"px";
     spanE1.style.top=yPos+"px";
-    const size= Math.floor(Math.random() * ( 541)) + 60;
+    const size= Math.floor(Math.random() * 441) + 60;
     spanE1.style.width=size+"px";
     spanE1.style.height=size+"px";
     bodyE1.appendChild(spanE1);
